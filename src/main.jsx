a36ed5a79e04241e8e98cc4a386574d9cdfb3b42
mdaliakbor <aliakbor.js@gmail.com>
2024-03-18 15:47:06 +0600

    history generate done

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+// import App1 from "./App1.jsx";
+import ProfileCartSection from "./profileCart/ProfileCartSection.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
+    {/* <App1 /> */}
+    <ProfileCartSection />
   </React.StrictMode>
 );
---
+++ src/profileCart/SocialMedia.jsx
@@ -0,0 +1,64 @@
+import {
+  FaBehance,
+  FaFacebookF,
+  FaGithub,
+  FaInstagram,
+  FaLinkedinIn,
+  FaTwitter,
+} from "react-icons/fa6";
+
+const SocialMedia = () => {
+  const social = [
+    {
+      id: "1d92e6",
+      icon: <FaFacebookF />,
+      link: "https://www.facebook.com/",
+    },
+    {
+      id: "1d92e5",
+      icon: <FaLinkedinIn />,
+      link: "https://www.linkedin.com/",
+    },
+    {
+      id: "1d92e4",
+      icon: <FaBehance />,
+      link: "https://www.behance.net/",
+    },
+    {
+      id: "1d92e3",
+      icon: <FaTwitter />,
+      link: "https://twitter.com/",
+    },
+    {
+      id: "1d92e2",
+      icon: <FaInstagram />,
+      link: "https://www.instagram.com/",
+    },
+    {
+      id: "1d92e1",
+      icon: <FaGithub />,
+      link: "https://github.com/",
+    },
+  ];
+
+  return (
+    <div className='bg-cyan-50 rounded-3xl h-full py-[150px]'>
+      <h4 className='text-[#454444] font-semibold text-center'>
+        See My Latest Work Here
+      </h4>
+      <div className='grid grid-cols-3 gap-8 px-10 py-6'>
+        {social.map((social) => (
+          <div
+            key={social.id}
+            className='text-3xl text-[#0866FF] border-2 border-[#0866FF] py-4 px-5 rounded-xl flex justify-center '
+          >
+            {/* <Link to={social.link}>{social.icon}</Link> */}
+          </div>
+        ))}
+      </div>
+      <p className='text-[#454444] font-semibold text-center '>Follow Me....</p>
+    </div>
+  );
+};
+
+export default SocialMedia;
